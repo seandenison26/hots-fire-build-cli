b3406b6edacb361197ef499b33ae198444fd5322
ziggy <sean.denison26@gmail.com>
2017-05-28 11:25:22 -0500

    Implemented Scraper.get_free_here_list

--- lib/hots_fire_build/scraper.rb
+++ lib/hots_fire_build/scraper.rb
@@ -1,3 +1,7 @@
 class Scraper
-
+		#Returns a list of the current free heroes.
+	def self.get_free_hero_list
+		home = Nokogiri::HTML(open("http://www.heroesfire.com/"))
+		list = home.css("div.free span.link-wrap a").map {|link| link.attr("href").gsub("/hots/wiki/heroes/", "").capitalize}
+	end
 end
